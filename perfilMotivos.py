8ca2bf4099a4e1f7c5e4021f8f83a3ab1cf67b07
João G. Ferreira <ferreiranunesjoao@gmail.com>
2018-04-11 18:52:10 -0300

    implementando a função perfilMotivos

--- perfilMotivos.py
+++ perfilMotivos.py
@@ -3,11 +3,28 @@ from contagemMotivos import contagem
 def perfil(matriz):
     num_sequencias = len(matriz)
     matriz_contagem = contagem(matriz)
+
+    print(matriz_contagem)
         
-    for i in matriz_contagem:
-        for j in i:
-            j = float(j)/num_sequencias
+    for i in range(len(matriz_contagem)):
+        for j in range(len(matriz_contagem[0])):
+            matriz_contagem[i][j] = matriz_contagem[i][j]/float(num_sequencias)
+
             
     return(matriz_contagem)
 
-print(perfil(["TTGGGGACTT","TTGCATAGGT","TTGGGGTATT","TTAAGGAAAT"]))
+if __name__ == '__main__':
+    matriz_contagem = perfil(["TTGGGGACTT","TTGCATAGGT","TTGGGGTATT","TTAAGGAAAT"])
+    
+    c = 0
+    for nucleotideo in matriz_contagem:
+        if c ==0:
+            print('A:', nucleotideo)
+        if c ==1:
+            print('T:', nucleotideo)
+        if c ==2:
+            print('C:', nucleotideo)
+        if c ==3:
+            print('G:', nucleotideo)
+            
+        c +=1
